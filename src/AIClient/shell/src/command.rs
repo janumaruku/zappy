50be79cfb7b3d2339988732e1c7d54d468fa2bdd
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-07 15:39:15 +0200

    feat: ZAP-19 #comment Create and implement CommandContext

--- src/AIClient/shell/src/command.rs
+++ src/AIClient/shell/src/command.rs
@@ -2,8 +2,10 @@ mod command_definition;
 mod positional_argument_builder;
 mod cmd_option_builder;
 mod cmd_flag_builder;
+mod command_context;
 
 pub use command_definition::*;
 pub use positional_argument_builder::*;
 pub use cmd_option_builder::*;
 pub use cmd_flag_builder::*;
+pub use command_context::*;
---
+++ src/AIClient/shell/src/command/command_context.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+pub struct CommandContext {
+    args: HashMap<String, String>,
+    options: HashMap<String, String>,
+    flags: Vec<String>,
+}
+
+impl CommandContext {
+    pub fn arg(&self, name: &str) -> Option<String> {
+        self.args.get(name).cloned()
+    }
+
+    pub fn has_arg(&self, name: &str) -> bool {
+        self.args.contains_key(name)
+    }
+
+    pub fn args_count(&self) -> usize {
+        self.args.len()
+    }
+
+    pub fn option(&self, name: &str) -> Option<String> {
+        self.options.get(name).cloned()
+    }
+
+    pub fn has_option(&self, name: &str) -> bool {
+        self.options.contains_key(name)
+    }
+
+    pub fn flag(&self, name: &str) -> bool {
+        self.flags.contains(&name.to_string())
+    }
+
+    pub fn add_arg(&mut self, name: &str, value: &str) {
+        self.args.insert(name.to_string(), value.to_string());
+    }
+
+    pub fn add_option(&mut self, name: &str, value: &str) {
+        self.options.insert(name.to_string(), value.to_string());
+    }
+
+    pub fn add_flag(&mut self, name: &str) {
+        self.flags.push(name.to_string());
+    }
+}
