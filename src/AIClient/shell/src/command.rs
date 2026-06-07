cd1db51ae19d614cc1287bdc10acecb46c933e65
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-07 23:10:24 +0200

    feat: ZAP-19 #comment Create command error types

--- src/AIClient/shell/src/command.rs
+++ src/AIClient/shell/src/command.rs
@@ -4,9 +4,11 @@ mod command_builder;
 mod command_context;
 mod command_definition;
 mod positional_argument_builder;
+mod command_error;
 
 pub use cmd_flag_builder::*;
 pub use cmd_option_builder::*;
 pub use command_context::*;pub use command_definition::*;
 pub use positional_argument_builder::*;
-
+pub use command_builder::*;
+pub use command_error::*;
