2c72adea8f2fbfaf105a625985137282e8f15ab2
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-08 02:20:34 +0200

    feat: ZAP-19 #comment Implement help formatter

--- src/AIClient/shell/src/command.rs
+++ src/AIClient/shell/src/command.rs
@@ -5,6 +5,7 @@ mod command_context;
 mod command_definition;
 mod positional_argument_builder;
 mod command_error;
+mod help_formatter;
 
 pub use cmd_flag_builder::*;
 pub use cmd_option_builder::*;
@@ -12,3 +13,4 @@ pub use command_context::*;pub use command_definition::*;
 pub use positional_argument_builder::*;
 pub use command_builder::*;
 pub use command_error::*;
+pub use help_formatter::*;
