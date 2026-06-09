c633fbc70de6cf805be8b260198335277f32f084
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-09 08:06:39 +0200

    refactor: ZAP-19 #comment Update color constants string literals

--- src/AIClient/useful/src/color.rs
+++ src/AIClient/useful/src/color.rs
@@ -1,7 +1,7 @@
-pub const RESET: &str = "\033[0m";
-pub const RED: &str = "\033[31m";
-pub const GREEN: &str = "\033[32m";
-pub const YELLOW: &str = "\033[33m";
-pub const BLUE: &str = "\033[34m";
-pub const MAGENTA: &str = "\033[35m";
-pub const CYAN: &str = "\033[36m";
+pub const RESET: &str = "\u{1b}[0m";
+pub const RED: &str = "\u{1b}[31m";
+pub const GREEN: &str = "\u{1b}[32m";
+pub const YELLOW: &str = "\u{1b}[33m";
+pub const BLUE: &str = "\u{1b}[34m";
+pub const MAGENTA: &str = "\u{1b}[35m";
+pub const CYAN: &str = "\u{1b}[36m";
