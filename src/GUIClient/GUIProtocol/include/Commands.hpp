ee6f01c01d0c04a00071ead5a94b4431ed5cbe3e
Anakondrall <leonardo.bonnard@gmail.com>
2026-06-09 15:03:14 +0200

    refractor(GUIClient): ZAP-11 #comment restructure file system for GUIProtocol

---
+++ src/GUIClient/GUIProtocol/include/Commands.hpp
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Commands
+*/
+
+#ifndef COMMANDS_HPP
+    #define COMMANDS_HPP
+    // #include "include/ICommand.hpp"
+
+// class MszCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getMsz() const;
+// };
+//
+// class MctCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getMct() const;
+// };
+//
+// class BctCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getBct() const;
+// };
+//
+// class TnaCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getTna() const;
+// };
+//
+// class PpoCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getPpo() const;
+// };
+//
+// class PlvCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getPlv() const;
+// };
+//
+// class PinCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getPin() const;
+// };
+//
+// class SgtCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getSgt() const;
+// };
+//
+// class SstCommand : ICommand {
+//     private:
+//         void execute(WorldState& worldState, const std::list<std::string>& args) = 0;
+//         void getSst() const;
+// };
+
+#endif
---
+++ src/GUIClient/GUIProtocol/include/ProtocolHandler.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** ProtocolHandler
+*/
+
+#ifndef ZAPPY_PROTOCOLHANDLER_HPP
+    #define ZAPPY_PROTOCOLHANDLER_HPP
+    #include <map>
+    #include <string>
+    #include <functional>
+    #include "ICommand.hpp"
+
+class ProtocolHandler {
+    private:
+        // WorldState& _worldState;
+        //
+        // ProtocolHandler() noexcept;
+        // std::map<std::string, std::function<ICommand*()>> _factories;
+        // void handleLine(const std::string line) noexcept;
+        // void registerCommand(const std::string &name,
+        //     std::function<ICommand*()> creator) noexcept;
+};
+#endif
