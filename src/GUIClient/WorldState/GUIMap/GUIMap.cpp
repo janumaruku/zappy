dd42bd17e0a1639f0faa0a0a6ce4e2bd95741f79
Tom Zhang <tom.zhang@epitech.eu>
2026-06-10 14:22:13 +0200

    feat(GUI): ZAP-13 #comment add GUIMap

---
+++ src/GUIClient/WorldState/GUIMap/GUIMap.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** GUIMap.cpp
+*/
+
+#include "GUIMap.hpp"
+
+#include <string>
+
+uint zappy::gui::GUIMap::getWidth() const
+{
+    return _width;
+}
+
+uint zappy::gui::GUIMap::getHeight() const
+{
+    return _height;
+}
+
+/*
+ *Tile getTile(Position pos) {
+ *
+ *}
+*/
+
+/*void updateTile(Position pos, map<Ressource, uint> ressources) {
+ *
+ *}
+ */
---
+++ src/GUIClient/WorldState/GUIMap/include/GUIMap.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** GUIMap.hpp
+*/
+
+#ifndef GUIMAP_HPP_H
+#define GUIMAP_HPP_H
+#include <sys/types.h>
+
+namespace zappy::gui {
+class GUIMap {
+public:
+    uint getWidth() const;
+
+    uint getHeight() const;
+
+    //Tile getTile(Position pos);
+    //void updateTile(Position pos, map<Ressource, uint> ressources);
+private:
+    uint _width = 0;
+    uint _height = 0;
+    //list<Tile> _tiles;
+};
+}
+
+#endif
