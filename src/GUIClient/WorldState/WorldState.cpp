f1703dc92689e47f0ffab18e0a8662826429f1a6
Tom Zhang <tom.zhang@epitech.eu>
2026-06-10 19:18:46 +0200

    feat(GUI): ZAP-13 #comment add empty function WorldState

--- src/GUIClient/WorldState/WorldState.cpp
+++ src/GUIClient/WorldState/WorldState.cpp
@@ -6,3 +6,54 @@
 */
 
 #include "WorldState.hpp"
+
+//GUIMap getMap(){
+//  return _map;
+//}
+
+//map<PlayerId, GUIPlayer> getPlayers(){
+//
+//}
+
+//GUIPlayer getPlayer(PlayerId id){
+//return _player.find(id);
+//}
+
+//map<string, Team> getTeams(){
+//return _teams;
+//}
+
+//void onPlayerNew(PlayerId id, Position pos, Orientation orientation, uint8 level, std::string team){
+//
+//}
+
+//void onPlayerPosition(Position pos, Orientation orientation){
+//
+//}
+
+//void onPlayerDeath(PlayerId id){
+//
+//}
+
+//void onTileContent(Position pos, map<Resource, uint> resources){
+//
+//}
+
+void onTimeUnit(uint t)
+{
+    (void)t;
+}
+
+uint zappy::gui::WorldState::getTimeUnit() const
+{
+    return _timeUnit;
+}
+
+//void onEggLaid(uint eggId, PlayerId playerId, Position pos){
+//
+//}
+
+void onEggDeath(uint eggId)
+{
+    (void)eggId;
+}
--- src/GUIClient/WorldState/include/WorldState.hpp
+++ src/GUIClient/WorldState/include/WorldState.hpp
@@ -7,12 +7,33 @@
 
 #ifndef WORLDSTATE_HPP_
 #define WORLDSTATE_HPP_
+#include <sys/types.h>
 
 namespace zappy::gui {
-    class WorldState {
-    public:
-        //GUIMap
-    };
+class WorldState {
+public:
+    //GUIMap getMap();
+    //map<PlayerId, GUIPlayer> getPlayers();
+    //GUIPlayer getPlayer(Playerid id);
+    //map<string, Team> getTeams();
+    uint getTimeUnit() const;
+
+    //void onPlayerNew(PlayerId id, Position pos, Orientation orientation, uint8 level, std::string team);
+    //void onPlayerPosition(Position pos, Orientation orientation);
+    //void onPlayerDeath(PlayerId id);
+    //void onTileContent(Position pos, map<Resource, uint> resources);
+    void onTimeUnit(uint t);
+
+    //void onEggLaid(uint eggId, PlayerId playerId, Position pos);
+    void onEggDeath(uint eggId);
+
+private:
+    //GUIMap _map;
+    //map<PlayerId, GUIPlayer> _players;
+    //map<uint, GUIEgg> _eggs;
+    //map<string, Team> _teams;
+    uint _timeUnit = 0;
+};
 }
 
 #endif
