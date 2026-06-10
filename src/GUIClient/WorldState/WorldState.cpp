1d2137a42b0403efa0057ba3323cdc5974426a04
Tom Zhang <tom.zhang@epitech.eu>
2026-06-10 14:21:45 +0200

    feat(GUI): ZAP-13 #comment add WorldState

---
+++ src/GUIClient/WorldState/WorldState.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** WorldState
+*/
+
+#include "WorldState.hpp"
---
+++ src/GUIClient/WorldState/include/WorldState.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** WorldState.hpp
+*/
+
+#ifndef WORLDSTATE_HPP_
+#define WORLDSTATE_HPP_
+
+namespace zappy::gui {
+    class WorldState {
+    public:
+        //GUIMap
+    };
+}
+
+#endif
