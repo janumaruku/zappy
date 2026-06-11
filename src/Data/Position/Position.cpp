7a322dfd4a0685d286680607de535eb1b645e7e9
Tom Zhang <tom.zhang@epitech.eu>
2026-06-11 15:22:36 +0200

    feat(Data): ZAP-13 #comment add Data Position

---
+++ src/Data/Position/Position.cpp
@@ -0,0 +1,78 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Position.cpp
+*/
+
+#include "Position.hpp"
+
+#include <stdexcept>
+
+namespace zappy::data {
+Position::Position(int x, int y): _x(x), _y(y)
+{
+
+}
+
+int Position::getX() const
+{
+    return _x;
+}
+
+int Position::getY() const
+{
+    return _y;
+}
+
+Position Position::operator+(Position other) const
+{
+    return {_x + other._x, _y + other._y};
+}
+
+void Position::operator+=(Position other)
+{
+    _x += other._x;
+    _y += other._y;
+}
+
+Position Position::operator-(Position other) const
+{
+    return {_x - other._x, _y - other._y};
+}
+
+void Position::operator-=(Position other)
+{
+    _x -= other._x;
+    _y -= other._y;
+}
+
+Position Position::operator*(double scalar) const
+{
+    return {static_cast<int>(_x * scalar), static_cast<int>(_y * scalar)};
+}
+
+void Position::operator*=(double scalar)
+{
+    _x *= scalar;
+    _y *= scalar;
+}
+
+Position Position::operator/(double scalar) const
+{
+    if (scalar == 0) {
+        throw std::runtime_error("Attempt to divide by 0");
+    }
+    return {static_cast<int>(_x / scalar), static_cast<int>(_y / scalar)};
+}
+
+void Position::operator/=(double scalar)
+{
+    if (scalar == 0) {
+        throw std::runtime_error("Attempt to divide by 0");
+    }
+    _x *= scalar;
+    _y *= scalar;
+}
+
+}
---
+++ src/Data/Position/include/Position.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Position.hpp
+*/
+
+#ifndef POSITION_HPP
+#define POSITION_HPP
+
+namespace zappy::data {
+class Position {
+public:
+    Position(int x, int y);
+
+    int getX() const;
+
+    int getY() const;
+
+    Position operator+(Position other) const;
+
+    void operator+=(Position other);
+
+    Position operator-(Position other) const;
+
+    void operator-=(Position other);
+
+    Position operator*(double scalar) const;
+
+    void operator*=(double scalar);
+
+    Position operator/(double scalar) const;
+
+    void operator/=(double scalar);
+
+private:
+    int _x;
+    int _y;
+};
+}
+
+#endif
