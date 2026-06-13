d267fb25c4f5524dffc407579849612a3167b126
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-13 11:52:28 +0200

    feat(behavior tree): ZAP-70 #comment Create the ActionNode and implement BehaviorNode trait for it.

--- src/AIClient/behavior_tree/src/behavior_tree.rs
+++ src/AIClient/behavior_tree/src/behavior_tree.rs
@@ -2,7 +2,9 @@ mod behavior_tree_node;
 mod sequence_node;
 mod selector_node;
 mod condition_node;
+mod action_node;
 
 pub use behavior_tree_node::*;
 pub use sequence_node::*;
 pub use selector_node::*;
+pub use condition_node::*;
