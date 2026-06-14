e89c5122b1f7fff2725c40d9eebca30452841437
Liam <liam.harfouche@epitech.eu>
2026-06-14 05:04:36 +0200

    feat(AISession): can split any command

--- src/Server/Session/AISession.cpp
+++ src/Server/Session/AISession.cpp
@@ -43,42 +43,37 @@ std::size_t AISession::getResultSize(const std::string &str)
 {
     std::size_t count = 0;
     std::size_t strSize = str.size();
-    size_t i = 0;
+    std::size_t i = 0;
 
-    for (; str[i]; i++) {
-        auto currentPos = i;
-        for (; i + 1 < strSize && !std::isalpha(str[i + 1]); i++);
-        count += currentPos != i;
+    for (; i < strSize; i++) {
+        if (!std::isalpha(str[i]))
+            continue;
+        count++;
+        for (; i != strSize && std::isalpha(str[i + 1]); i++);
     }
     return count;
 }
 
-
-std::vector<std::string> AISession::sanitizedSplit(const std::string &str) 
+std::vector<std::string> AISession::sanitizedSplit(const std::string &str)
 {
     std::vector<std::string> result;
 
     if (str == "\n") {
-        result.emplace_back ("\n");
+        result.emplace_back("\n");
         return result;
     }
 
-    std::string buf;
     std::size_t resultSize = getResultSize(str);
-    size_t j = 0;
+    std::size_t strSize = str.size();
+    std::string buf;
+    std::size_t j = 0;
 
-    for (size_t i = 0; i != resultSize; i++) {
-        for (; str[j]; j++) {
-            if (!std::isalpha(str[j])){
-                j++;
-                break;
-            }
-            std::cout << "current char:\"" << str[j] << "\"" << std::endl;
-            buf.push_back(str[j]);
-        }
+    for (std::size_t i = 0; i != resultSize; i++) {
+        for (; j < strSize && !std::isalpha(str[j]); j++);
+        while (j < strSize && std::isalpha(str[j]))
+            buf.push_back(str[j++]);
         result.push_back(buf);
         buf.clear();
-        std::cout << "vec[i] = " << result[i] << std::endl;
     }
     return result;
 }
