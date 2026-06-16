654dad5fb5873babd54b24e368177e3f157220d2
Tom Zhang <tom.zhang@epitech.eu>
2026-06-16 16:31:08 +0200

    feat(GUI): ZAP-14 #comment implemented Renderer class within Core

--- src/ZappyGUIClient/Core/Core.cpp
+++ src/ZappyGUIClient/Core/Core.cpp
@@ -15,6 +15,7 @@
 
 namespace zappy::gui {
 Core::Core(char **argv)
+// add renderer class
 {
     for (auto i = 0; argv[i] != nullptr; i++)
         _argv.emplace_back(argv[i]);
@@ -25,7 +26,6 @@ void Core::run()
     buildGUICommands();
 
     _guiCommands.run(std::move(_argv));
-    _renderer.loadTextures();
     while (_renderer.isWindowOpen()) {
         _renderer.render(_worldState);
     }
@@ -35,24 +35,24 @@ void Core::buildGUICommands()
 {
     _guiCommands =
         shell::command::CommandBuilder()
-        .name("./zappy_GUI")
-        .description("Run the GUI client with the specified arguments.")
-        .option([](shell::command::OptionBuilder &builder) {
-            builder.name("port")
-                .alias("p")
-                .required()
-                .description("port linked to the server");
-        })
-        .option([](shell::command::OptionBuilder &builder) {
-            builder.name("machine")
-                .alias("m")
-                .required()
-                .description("hostname");
-        })
-        .action([this](const shell::command::CommandContext &ctx) {
-            _tcpClient = std::make_unique<TCPClient>(_ioc,
-                std::stoi(ctx.option("port")), ctx.option("machine"));
-        })
-        .build();
+            .name("./zappy_GUI")
+            .description("Run the GUI client with the specified arguments.")
+            .option([](shell::command::OptionBuilder &builder) {
+                builder.name("port")
+                    .alias("p")
+                    .required()
+                    .description("port linked to the server");
+            })
+            .option([](shell::command::OptionBuilder &builder) {
+                builder.name("machine")
+                    .alias("m")
+                    .required()
+                    .description("hostname");
+            })
+            .action([this](const shell::command::CommandContext &ctx) {
+                _tcpClient = std::make_unique<TCPClient>(_ioc,
+                    std::stoi(ctx.option("port")), ctx.option("machine"));
+            })
+            .build();
 }
 } // namespace zappy::gui
