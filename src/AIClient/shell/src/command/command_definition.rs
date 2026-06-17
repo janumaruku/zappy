fdc176ba23d85cadcb42491da0ff8f8221d70e68
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-17 21:49:37 +0200

    feat(ai): ZAP-77 #comment Implement command line argument parsing

--- src/AIClient/shell/src/command/command_definition.rs
+++ src/AIClient/shell/src/command/command_definition.rs
@@ -143,26 +143,25 @@ impl CommandDefinition {
     }
 
     pub fn run(&self, tokens: &[String]) {
+        if tokens.contains(&"--help".to_string()) || tokens.contains(&"-h".to_string()) {
+            if tokens.len() == 2 {
+                println!("{}", format_help(&self));
+                return;
+            } else {
+                eprintln!(
+                    "{}Error: {}see {} --help",
+                    useful::color::RED,
+                    useful::color::RESET,
+                    self.name
+                );
+                return;
+            }
+        }
+
         let context = self.build_context(tokens);
 
         match context {
-            Ok(context) => match context.flag("help") {
-                true => {
-                    if context.args_count() != 0
-                        || context.flags_count() != 1
-                    {
-                        eprintln!(
-                            "{}Error: {}see {} --help",
-                            useful::color::RED,
-                            useful::color::RESET,
-                            self.name
-                        )
-                    } else {
-                        println!("{}", format_help(&self))
-                    }
-                }
-                false => (self.handler)(&context),
-            },
+            Ok(context) => (self.handler)(&context),
             Err(error) => {
                 eprintln!("{error}. See {} --help", self.name);
             }
---
+++ src/AIClient/zappy_ai/src/main.rs
@@ -0,0 +1,57 @@
+use shell::command::{CommandBuilder, CommandDefinition};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn build_command(
+    port: Rc<RefCell<i32>>,
+    team: Rc<RefCell<String>>,
+    hostname: Rc<RefCell<String>>,
+) -> Result<CommandDefinition, String> {
+    CommandBuilder::new()
+        .name("zappy_ai")
+        .description("Zappy AI agent. Drives the player on the map")
+        .option(|builder| {
+            builder
+                .name("port")
+                .alias('p')
+                .description("Port number")
+                .required()
+        })
+        .option(|builder| {
+            builder
+                .name("name")
+                .alias('n')
+                .description("Port number")
+                .required()
+        })
+        .option(|builder| {
+            builder
+                .name("hostname")
+                .alias('m')
+                .description("Port number")
+                .required()
+        })
+        .action(move |context| {
+            *port.borrow_mut() = context.option("port").unwrap().parse().unwrap();
+            *team.borrow_mut() = context.option("name").unwrap().to_string();
+            *hostname.borrow_mut() = context.option("hostname").unwrap().to_string();
+            println!("Port:\t\t{}", port.borrow());
+            println!("Team:\t\t{}", team.borrow());
+            println!("Hostname:\t\t{}", hostname.borrow());
+        })
+        .build()
+}
+
+fn main() {
+    let port = Rc::new(RefCell::new(0));
+    let team = Rc::new(RefCell::new(String::new()));
+    let hostname = Rc::new(RefCell::new(String::new()));
+
+    let command = build_command(port.clone(), team.clone(), hostname.clone());
+    let tokens: Vec<String> = std::env::args().collect();
+    command.unwrap().run(&tokens);
+
+    // println!("Port:\t\t{}", port.borrow());
+    // println!("Team:\t\t{}", team.borrow());
+    // println!("Hostname:\t\t{}", hostname.borrow());
+}
