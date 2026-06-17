93e28f7cf021f7c6234bc2fa395322275c2e7871
Servallou <lotomunecraft@gmail.com>
2026-06-17 17:23:49 +0200

    refactor(Server): Refactored the Look command

--- src/ZappyServer/Server/Commands/LookCommand.cpp
+++ src/ZappyServer/Server/Commands/LookCommand.cpp
@@ -12,13 +12,136 @@
 #include <vector>
 #include "AIProtocolHandler.hpp"
 #include "AISession.hpp"
+#include "Server.hpp"
 #include "LookCommand.hpp"
 
 namespace zappy::server {
 
-bool LookCommand::execute(AISession& s, const std::vector<std::string>& v)
+static int wrapCoord(int value, int max)
 {
-    s.send(v[0]);
+    if (max <= 0)
+        return value;
+    int r = value % max;
+    if (r < 0)
+        r += max;
+    return r;
+}
+
+static const std::map<data::Resource, std::string> &resourceNames()
+{
+    static const std::map<data::Resource, std::string> names = {
+        {data::Resource::FOOD, "food"},
+        {data::Resource::LINEMATE, "linemate"},
+        {data::Resource::DERAUMERE, "deraumere"},
+        {data::Resource::SIBUR, "sibur"},
+        {data::Resource::MENDIANE, "mendiane"},
+        {data::Resource::PHIRAS, "phiras"},
+        {data::Resource::THYSTAME, "thystame"},
+    };
+    return names;
+}
+
+static std::vector<data::Position> buildFOVPositions(const data::Position &playerPos,
+    data::Orientation orientation, std::uint8_t level, int mapWidth, int mapHeight)
+{
+    std::vector<data::Position> positions;
+    positions.reserve(static_cast<std::uint8_t>((level + 1) * (level + 1) * 2));
+
+    for (int r = 0; r <= static_cast<int>(level); ++r) {
+        for (int i = -r; i <= r; ++i) {
+            int x = playerPos.getX();
+            int y = playerPos.getY();
+
+            switch (orientation) {
+            case data::Orientation::UP:
+                x = playerPos.getX() + i;
+                y = playerPos.getY() - r;
+                break;
+            case data::Orientation::RIGHT:
+                x = playerPos.getX() + r;
+                y = playerPos.getY() + i;
+                break;
+            case data::Orientation::DOWN:
+                x = playerPos.getX() - i;
+                y = playerPos.getY() + r;
+                break;
+            case data::Orientation::LEFT:
+                x = playerPos.getX() - r;
+                y = playerPos.getY() - i;
+                break;
+            }
+
+            x = wrapCoord(x, mapWidth);
+            y = wrapCoord(y, mapHeight);
+
+            positions.emplace_back(x, y);
+        }
+    }
+
+    return positions;
+}
+
+bool LookCommand::execute(AISession& session, const std::vector<std::string>& v)
+{
+    session.send(v[0]);
+    const Server &server = session.getServer();
+    const Player &player = session.getPlayer();
+    const Map &map = server.getMap();
+
+    const auto playerPos = player.getPosition();
+    const auto level = player.getLevel();
+    const auto orientation = player.getOrientation(); 
+
+    const int mapWidth = map.getWidth();
+    const int mapHeight = map.getHeight();
+
+    const auto positions = buildFOVPositions(playerPos, orientation, level,
+        mapWidth, mapHeight);
+
+    std::ostringstream response;
+    response << "[";
+
+    const auto &resNames = resourceNames();
+
+    bool firstTile = true;
+    for (const auto &pos : positions) {
+        if (!firstTile)
+            response << ", ";
+        firstTile = false;
+
+        const data::Tile &tile = map.getTile(pos);
+
+        std::vector<std::string> contents;
+
+        if (pos.getX() == playerPos.getX() && pos.getY() == playerPos.getY()) {
+            contents.emplace_back("player");
+        }
+
+        const auto &resources = tile.getResources();
+        for (const auto &kv : resources) {
+            const auto &resource = kv.first;
+            const auto count = kv.second;
+            if (count == 0)
+                continue;
+
+            auto it = resNames.find(resource);
+            if (it == resNames.end())
+                continue;
+
+            for (unsigned int c = 0; c < count; ++c)
+                contents.emplace_back(it->second);
+        }
+
+        for (size_t i = 0; i < contents.size(); ++i) {
+            if (i != 0)
+                response << " ";
+            response << contents[i];
+        }
+    }
+
+    response << "]\n";
+
+    session.scheduleResponse(7, response.str());
     return true;
 }
 
