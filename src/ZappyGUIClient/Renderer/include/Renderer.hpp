0e88643c252ff7617e481439bb95750810e2fdd5
Tom Zhang <tom.zhang@epitech.eu>
2026-06-17 04:18:51 +0200

    feat(GUI): ZAP-60 #comment add loadTexture and loadImages function in Renderer class (Images are used to create Textures)

--- src/ZappyGUIClient/Renderer/include/Renderer.hpp
+++ src/ZappyGUIClient/Renderer/include/Renderer.hpp
@@ -15,25 +15,37 @@
 
 namespace zappy::gui {
 
-
 class Renderer {
-    const float TILE_SIZE = 10;
+    const int TILE_SIZE = 50;
+
 public:
     Renderer();
+
     ~Renderer();
 
-    static void loadTextures();
+    void loadTextures();
+
     void render(const WorldState &world) const;
+
     bool isWindowOpen();
 
 private:
     static void renderMap(const GUIMap &map);
+
     static void renderPlayers(const std::map<data::PlayerId, GUIPlayer>
-            &players);
+        &players);
+
     static void renderEggs(const std::map<unsigned int, data::Egg> &eggs);
+
     static void updateAnimation(const GUIPlayer &player);
+
     Vector2 tileToPixel(data::Position pos) const;
 
+    Image createImageResource(Color color) const;
+
+    void loadImages();
+
+    std::unordered_map<std::string, Image> _images;
     std::map<std::string, Texture2D> _textures;
     // std::map<data::PlayerId, Animation> _animations;
     Camera2D _camera;
