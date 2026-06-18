36a77784e5a14fe8f1718b42b57687c5aa0a7e23
Tom Zhang <tom.zhang@epitech.eu>
2026-06-18 03:43:28 +0200

    feat(GUI): ZAP-60 #comment implementation of renderMap function inside Render Class, also removed ResourceManager function and variables from Renderer

--- src/ZappyGUIClient/Renderer/ResourceManager.cpp
+++ src/ZappyGUIClient/Renderer/ResourceManager.cpp
@@ -45,14 +45,12 @@ void ResourceManager::loadImages()
         const data::Position &pos, const int &radius, const Color &c) {
         _images.insert({name, createImageResource(pos, radius, c)});
     };
-    insertImage("food", {0, 0}, RESOURCE_RADIUS, RED);
-    insertImage("linemate", {CELL_SIZE, 0}, RESOURCE_RADIUS, LIME);
-    insertImage("deraumere", {CELL_SIZE * 2, 0}, RESOURCE_RADIUS,BLUE);
-    insertImage("sibur", {0, CELL_SIZE}, RESOURCE_RADIUS, YELLOW);
-    insertImage("egg", {CELL_SIZE, CELL_SIZE}, RESOURCE_RADIUS,BLACK);
-    insertImage("mendiane", {CELL_SIZE * 2, CELL_SIZE}, RESOURCE_RADIUS,VIOLET);
-    insertImage("phiras", {0, CELL_SIZE * 2}, RESOURCE_RADIUS,ORANGE);
-    insertImage("thystame", {CELL_SIZE, CELL_SIZE * 2}, RESOURCE_RADIUS, BROWN);
+    for (const auto &it: RESOURCE_DATA) {
+        data::Position position = {(it.pos.getX() * CELL_SIZE) + GRID_SIZE,
+                                   (it.pos.getY() * CELL_SIZE) + GRID_SIZE};
+        insertImage(it.name, position, RESOURCE_RADIUS, it.color);
+    }
+    insertImage("egg", {CELL_SIZE, CELL_SIZE}, RESOURCE_RADIUS, BLACK);
     _images.insert({"tile", GenImageColor(TILE_SIZE, TILE_SIZE, GRAY)});
 }
 
