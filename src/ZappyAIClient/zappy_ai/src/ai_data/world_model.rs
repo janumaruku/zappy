716821403c62adae6a73ec2068b138e70a320bc3
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-19 22:48:17 +0200

    feat(ai): ZAP-144 #comment Create server responses parser

--- src/ZappyAIClient/zappy_ai/src/ai_data/world_model.rs
+++ src/ZappyAIClient/zappy_ai/src/ai_data/world_model.rs
@@ -1,14 +1,24 @@
-use std::collections::HashMap;
 use behavior_tree::behavior_tree::BlackBoard;
+use std::collections::HashMap;
 
 #[derive(Eq, Hash, PartialEq)]
 pub enum Resource {
+    Food,
     Linemate,
     Deraumere,
     Sibur,
     Mendiane,
     Phiras,
     Thystame,
+    Unknown(String),
+}
+
+pub enum Orientation {
+    Up,
+    Right,
+    Down,
+    Left,
+    Unknown(u8),
 }
 
 pub struct WorldModel {
@@ -17,6 +27,33 @@ pub struct WorldModel {
     inventory: HashMap<Resource, u32>,
     map_with: u32,
     map_height: u32,
+}
+
+impl Resource {
+    pub fn from(str: &str) -> Self {
+        match str {
+            "food" => Resource::Food,
+            "linemate" => Resource::Food,
+            "deraumere" => Resource::Food,
+            "sibur" => Resource::Food,
+            "mendiane" => Resource::Food,
+            "phiras" => Resource::Food,
+            "thystame" => Resource::Food,
+            s => Resource::Unknown(s.to_string()),
+        }
+    }
+}
+
+impl Orientation {
+    pub fn from(num: u8) -> Self {
+        match num {
+            0 => Orientation::Up,
+            1 => Orientation::Right,
+            2 => Orientation::Down,
+            3 => Orientation::Left,
+            n => Orientation::Unknown(n),
+        }
+    }
 }
 
 impl WorldModel {
