239c20a3adb43cd602b780aa1d787c01fe22118a
janumaruku <jean-marc.kottin@epitech.eu>
2026-06-20 09:24:48 +0200

    feat(ai): ZAP-144 #comment Make types Orientation, Resource and ServerMessage derive the trait Clone and change the type of the function update, implemented on WorldModel to take ServerMessage instead of &str

--- src/ZappyAIClient/zappy_ai/src/ai_data.rs
+++ src/ZappyAIClient/zappy_ai/src/ai_data.rs
@@ -8,6 +8,7 @@ use ai_tcp_client::AiTcpClient;
 use behavior_tree::behavior_tree::{BehaviorTree, BlackBoard};
 pub use world_model::*;
 pub use zappy_tree_nodes::*;
+pub use server_message::*;
 
 pub fn build_tree(client: Rc<RefCell<AiTcpClient>>) -> BehaviorTree {
     BehaviorTree::new(random_walk(client.clone()))
