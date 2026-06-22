1521fa78632e7664a74090f25819514713f0ed1c
Tom Zhang <tom.zhang@epitech.eu>
2026-06-22 14:08:19 +0200

    fix(GUI): ZAP-138 #comment playerDropdownbutton can now be pressed (for now only show message that it's pressed)

--- src/ZappyGUIClient/Renderer/Hud.cpp
+++ src/ZappyGUIClient/Renderer/Hud.cpp
@@ -14,22 +14,47 @@
 
 namespace zappy::gui {
 
+void HUD::update(const std::unique_ptr<HUDManager> &hudManager)
+{
+    if (!_dropdownOpen) {
+        onDropdownButton(hudManager);
+    }
+}
 void HUD::draw(const WorldState &world,
     const std::unique_ptr<HUDManager> &hudManager) const
 {
     size_t count = 0;
 
     for (const auto &it: world.getTeams()) {
-        drawTeamsPanel(
-            world.getPlayers(), it, static_cast<int>(count),hudManager);
+        drawTeamsPanel(world.getPlayers(), it, static_cast<int>(count),
+            hudManager);
         ++count;
     }
     drawTime(world.getTimeUnit());
     drawPlayerSelectorDropdownButton(hudManager);
 }
 
-void HUD::drawTeamsPanel(
-    const std::unordered_map<data::PlayerId, GUIPlayer> &players,
+bool HUD::isDropdownOpen() const
+{
+    return _dropdownOpen;
+}
+
+void HUD::onDropdownButton(const std::unique_ptr<HUDManager> &hudManager)
+{
+    const Vector2 &mousePosition = GetMousePosition();
+    const Rectangle &dropdownButton =
+        hudManager->getRectangle(SELECT_DROPDOWN_BUTTON_NAME);
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+        CheckCollisionPointRec(mousePosition, dropdownButton)) {
+        _dropdownOpen = true;
+        // drawPlayerSelectorDropdownButton(hudManager);
+        std::cout << "dropdown button pressed" << std::endl;
+    }
+}
+
+void HUD::drawTeamsPanel(const std::unordered_map<data::PlayerId, GUIPlayer>
+                             &players,
     const std::pair<const std::string, Team> &team, const int &count,
     const std::unique_ptr<HUDManager> &hudManager)
 {
@@ -60,18 +85,20 @@ void HUD::drawTime(const int &timeUnit)
 void HUD::drawPlayerSelectorDropdownButton(const std::unique_ptr<HUDManager>
         &hudManager) const
 {
-    const data::Position position = {BACKGROUND_TEAM_WIDTH + TEXT_FONT_SIZE,
+    const data::Position textPos = {
+        BACKGROUND_DROPDOWN_BUTTON_X + TEXT_FONT_SIZE,
         (BACKGROUND_DROPDOWN_BUTTON_HEIGHT - TEXT_FONT_SIZE) / 2};
-
-    DrawTexture(hudManager->getTexture("bkgPlayerDropdown"),
-        BACKGROUND_TEAM_WIDTH, 0, GRAY);
+    const Rectangle &selectDropdownButton =
+        hudManager->getRectangle(SELECT_DROPDOWN_BUTTON_NAME);
+    DrawTexture(hudManager->getTexture(SELECT_DROPDOWN_BUTTON_NAME),
+        selectDropdownButton.x, selectDropdownButton.y, GRAY);
 
     if (!_selectedPlayerId.has_value()) {
-        DrawText("Select Player", position.getX(), position.getY(),
+        DrawText("Select Player", textPos.getX(), textPos.getY(),
             TEXT_FONT_SIZE, WHITE);
     } else {
-        DrawText(_selectedPlayerId.value().c_str(), position.getX(),
-            position.getY(), TEXT_FONT_SIZE, WHITE);
+        DrawText(_selectedPlayerId.value().c_str(), textPos.getX(),
+            textPos.getY(), TEXT_FONT_SIZE, WHITE);
     }
 }
 
--- src/ZappyGUIClient/Renderer/include/HudManager.hpp
+++ src/ZappyGUIClient/Renderer/include/HudManager.hpp
@@ -14,10 +14,18 @@ namespace zappy::gui {
 
 constexpr int BACKGROUND_TEAM_WIDTH  = 80;
 constexpr int BACKGROUND_TEAM_HEIGHT = 30;
+
+constexpr int BACKGROUND_DROPDOWN_BUTTON_X = BACKGROUND_TEAM_WIDTH;
+constexpr int BACKGROUND_DROPDOWN_BUTTON_Y = 0;
+
 constexpr int BACKGROUND_DROPDOWN_BUTTON_WIDTH  = 100;
 constexpr int BACKGROUND_DROPDOWN_BUTTON_HEIGHT = 50;
+
 constexpr int PLAYER_COUNT_Y_OFFSET = 10;
+
 constexpr int TEXT_FONT_SIZE = 5;
+
+const std::string SELECT_DROPDOWN_BUTTON_NAME = "PlayerSelectDropdownButton";
 
 class HUDManager: public designPattern::ARenderManager {
 public:
@@ -26,6 +34,7 @@ public:
 
 protected:
     void loadImages() override;
+    void loadRectangles() override;
 };
 } // namespace zappy::gui
 
