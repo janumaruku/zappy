a74c95a157ee70c5aebf653969a83cc0b7756c68
DarkcrossPrime <darkcrossmail@gmail.com>
2026-06-23 11:57:34 +0200

    feat(server): ZAP-95 #comment added Sgt command

---
+++ src/ZappyServer/Server/Commands/SgtCommand.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2025
+** ZPY
+** File description:
+** SgtCommand.cpp
+*/
+
+#include "SgtCommand.hpp"
+
+#include <memory>
+#include <string>
+
+#include "GUISession.hpp"
+#include "Server.hpp"
+
+namespace zappy::server {
+
+bool SgtCommand::execute(GUISession &session, const std::vector<std::string> &)
+{
+    session.send("sgt " + std::to_string(session.getServer().getFrequency()) + "\n");
+    return true;
+}
+
+bool SgtCommand::operator()(GUISession &session, const std::vector<std::string> &args)
+{
+    return execute(session, args);
+}
+
+std::unique_ptr<GUIProtocolCommand> SgtCommand::create()
+{
+    return std::make_unique<SgtCommand>();
+}
+
+}
---
+++ src/ZappyServer/Server/Commands/include/SgtCommand.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2025
+** ZPY
+** File description:
+** SgtCommand.hpp
+*/
+
+#pragma once
+
+#include "GUIProtocolHandler.hpp"
+
+namespace zappy::server {
+
+class SgtCommand : public GUIProtocolCommand {
+public:
+    SgtCommand() = default;
+    ~SgtCommand() override = default;
+
+    bool operator()(GUISession &, const std::vector<std::string> &) override;
+    bool execute(GUISession &, const std::vector<std::string> &) override;
+
+    static std::unique_ptr<GUIProtocolCommand> create();
+};
+
+}
