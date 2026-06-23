a8db99bf3b4754844ed1611c396f91be4b87f41c
Servallou <lotomunecraft@gmail.com>
2026-06-23 19:03:59 +0200

    feat(Command): ZAP-107 #comment Added PdiCommand in server side

--- src/ZappyServer/Server/GUIProtocolHandler.cpp
+++ src/ZappyServer/Server/GUIProtocolHandler.cpp
@@ -11,6 +11,7 @@
 #include <exception>
 
 #include "MszCommand.hpp"
+#include "PdiCommand.hpp"
 #include "PinCommand.hpp"
 #include "PnwCommand.hpp"
 #include "PlvCommand.hpp"
@@ -30,6 +31,7 @@ GUIProtocolHandler::~GUIProtocolHandler() = default;
 void GUIProtocolHandler::registerCreators() noexcept
 {
     _factory.registerCreator<MszCommand>("msz");
+    _factory.registerCreator<PdiCommand>("pdi");
     _factory.registerCreator<PinCommand>("pin");
     _factory.registerCreator<PnwCommand>("pnw");
     _factory.registerCreator<PlvCommand>("plv");
