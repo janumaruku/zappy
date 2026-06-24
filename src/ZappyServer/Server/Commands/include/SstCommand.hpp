d07b4da5f259727ce49dae851664edded4827b63
DarkcrossPrime <darkcrossmail@gmail.com>
2026-06-24 13:57:51 +0200

    feat(server): ZAP-96 #comment added Sst command

---
+++ src/ZappyServer/Server/Commands/include/SstCommand.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2025
+** ZPY
+** File description:
+** SstCommand.hpp
+*/
+
+#pragma once
+
+#include "GUIProtocolHandler.hpp"
+
+namespace zappy::server {
+
+class SstCommand : public GUIProtocolCommand {
+public:
+    SstCommand() = default;
+    ~SstCommand() override = default;
+
+    bool operator()(GUISession &, const std::vector<std::string> &) override;
+    bool execute(GUISession &, const std::vector<std::string> &) override;
+
+    static std::unique_ptr<GUIProtocolCommand> create();
+};
+
+}
