cb378316664d3998317a8cae99addc2c1b925a08
Liam <liam.harfouche@epitech.eu>
2026-06-25 17:12:59 +0200

    fix: removed const cast and added auto to remove redundancy

--- src/ZappyServer/Server/Commands/SetCommand.cpp
+++ src/ZappyServer/Server/Commands/SetCommand.cpp
@@ -51,8 +51,8 @@ bool SetCommand::execute(AISession &session,
     }
 
     const auto resource = it->second;
-    Server &server = const_cast<Server &>(session.getServer());
-    Player &player = const_cast<Player &>(session.getPlayer());
+    auto &server = session.getServer();
+    auto &player = session.getPlayer();
 
     if (!player.removeResource(resource)) {
         session.scheduleResponse(TIME_LIMIT, "ko\n");
