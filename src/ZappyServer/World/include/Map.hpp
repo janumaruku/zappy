e3a73f739f884f988937b919d7f0aa30ff6424a0
Servallou <lotomunecraft@gmail.com>
2026-06-25 17:37:11 +0200

    fix(Eject): ZAP-87#comment Fixed several things
    - De-commented egg removal code
    - Notify server when an egg is removed
    - Send actual orientation to the GUI instead of a hardcoded value

--- src/ZappyServer/World/include/Map.hpp
+++ src/ZappyServer/World/include/Map.hpp
@@ -41,7 +41,7 @@ public:
     void addEgg(const data::Egg &egg);
     void removeEgg(const uint &eggId);
     [[nodiscard]] const std::unordered_map<uint, data::Egg> &getEggs() const noexcept;
-    [[nodiscard]] std::vector<uint> getEggsOnTile(const data::Position &pos) const;
+    [[nodiscard]] const std::vector<uint> &getEggsOnTile(const data::Position &pos) const;
 
 private:
     int _width;
